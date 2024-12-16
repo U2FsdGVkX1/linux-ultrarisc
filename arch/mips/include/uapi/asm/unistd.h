a9f917ef18d6538427993b986947b9a608d6319a
Kexy Biscuit <kexybiscuit@aosc.io>
2024-12-17 05:45:49 +0800

    AOSCOS: MIPS: add copyright headers back
    In "FROMLIST: MIPS: Add syscall auditing support", a copyright header
    was removed from arch/mips/include/uapi/asm/unistd.h with no proper
    reason or related changes. Adding them back now.
    
    Signed-off-by: Kexy Biscuit <kexybiscuit@aosc.io>

--- arch/mips/include/uapi/asm/unistd.h
+++ arch/mips/include/uapi/asm/unistd.h
@@ -6,6 +6,9 @@
  *
  * Copyright (C) 1995, 96, 97, 98, 99, 2000 by Ralf Baechle
  * Copyright (C) 1999, 2000 Silicon Graphics, Inc.
+ *
+ * Changed system calls macros _syscall5 - _syscall7 to push args 5 to 7 onto
+ * the stack. Robin Farine for ACN S.A, Copyright (C) 1996 by ACN S.A
  */
 #ifndef _UAPI_ASM_UNISTD_H
 #define _UAPI_ASM_UNISTD_H
